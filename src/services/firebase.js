0b5c1e3829e87e8ddebe2fc25dde32d6714ac498
arefaat-arabyads <arefaat@arabyads.com>
2022-11-18 18:45:42 +0200

    add firebase config

---
+++ src/services/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbQ85Nq3zr5LlL7kEORTFNR9bgKoeHmeQ",
+  authDomain: "various-ideas.firebaseapp.com",
+  projectId: "various-ideas",
+  storageBucket: "various-ideas.appspot.com",
+  messagingSenderId: "1072231740689",
+  appId: "1:1072231740689:web:3c9f4037409ab795e648b0",
+  measurementId: "G-XE5ZM202Q4"
+};
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export {
+  db
+}
+
